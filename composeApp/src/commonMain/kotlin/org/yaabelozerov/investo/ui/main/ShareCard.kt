6a4f62321570d99c355576ede0f0a8c41c2e3a28
Yaroslav Belozerov <yaabelozerov@edu.hse.ru>
2025-01-02 21:35:33 +0300

    better chip ui

--- composeApp/src/commonMain/kotlin/org/yaabelozerov/investo/ui/main/ShareCard.kt
+++ composeApp/src/commonMain/kotlin/org/yaabelozerov/investo/ui/main/ShareCard.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
@@ -34,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.ImeAction
@@ -47,15 +49,11 @@ import org.yaabelozerov.investo.ui.main.model.ShareModel
 fun ShareCard(share: ShareModel) {
     AnimatedVisibility(share.isLoaded, enter = slideInVertically() + fadeIn()) {
         ElevatedCard(
-            modifier = Modifier
-                .fillMaxWidth()
-                .animateContentSize()
+            modifier = Modifier.fillMaxWidth().animateContentSize()
                 .clip(MaterialTheme.shapes.medium)
         ) {
             Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Column(
@@ -74,41 +72,15 @@ fun ShareCard(share: ShareModel) {
                         horizontalArrangement = Arrangement.spacedBy(8.dp),
                         verticalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
-                        if (share.country.isNotBlank()) {
-                            OutlinedCard(shape = MaterialTheme.shapes.small) {
-                                Text(
-                                    modifier = Modifier.padding(8.dp, 4.dp),
-                                    text = share.country
-                                )
-                            }
-                        }
-                        if (share.canShort) OutlinedCard(shape = MaterialTheme.shapes.small) {
-                            Text(modifier = Modifier.padding(8.dp, 4.dp), text = "Short")
-                        }
-                        if (share.apiTrade) OutlinedCard(shape = MaterialTheme.shapes.small) {
-                            Text(modifier = Modifier.padding(8.dp, 4.dp), text = "API")
-                        }
-                        if (share.canBuy) OutlinedCard(
-                            shape = MaterialTheme.shapes.small,
-                            colors = CardDefaults.outlinedCardColors()
-                                .copy(containerColor = MaterialTheme.colorScheme.primaryContainer)
-                        ) {
-                            Text(
-                                modifier = Modifier.padding(8.dp, 4.dp),
-                                text = "Buy",
-                                color = MaterialTheme.colorScheme.onPrimaryContainer
+                        if (share.country.isNotBlank()) ShareChip(share.country)
+                        if (share.canShort) ShareChip("Short")
+                        when {
+                            share.canSell && share.canBuy -> ShareChip(
+                                "Sell" to MaterialTheme.colorScheme.error,
+                                "Buy" to MaterialTheme.colorScheme.primary
                             )
-                        }
-                        if (share.canSell) OutlinedCard(
-                            shape = MaterialTheme.shapes.small,
-                            colors = CardDefaults.outlinedCardColors()
-                                .copy(containerColor = MaterialTheme.colorScheme.errorContainer)
-                        ) {
-                            Text(
-                                modifier = Modifier.padding(8.dp, 4.dp),
-                                text = "Sell",
-                                color = MaterialTheme.colorScheme.onErrorContainer
-                            )
+                            share.canSell -> ShareChip("Sell" to MaterialTheme.colorScheme.error)
+                            share.canBuy -> ShareChip("Buy" to MaterialTheme.colorScheme.primary)
                         }
                     }
                 }
@@ -125,3 +97,34 @@ fun ShareCard(share: ShareModel) {
         }
     }
 }
+
+@Composable
+private fun ShareChip(
+    vararg indicators: Pair<String, Color>,
+    background: Color = CardDefaults.cardColors().containerColor
+) {
+    Card(
+        shape = MaterialTheme.shapes.small,
+        colors = CardDefaults.cardColors(containerColor = background)
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp, 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            indicators.forEach {
+                Text(
+                    text = it.first, color = it.second
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ShareChip(
+    vararg indicators: String, background: Color = CardDefaults.cardColors().containerColor
+) = ShareChip(
+    *indicators.map { it to MaterialTheme.colorScheme.onBackground }.toTypedArray(),
+    background = background
+)

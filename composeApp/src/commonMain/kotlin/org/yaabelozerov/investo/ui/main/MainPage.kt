9fcfe6f9d97b9ea97b072b9180cf1f2bce3b6ec1
Yaroslav Belozerov <yaabelozerov@edu.hse.ru>
2025-01-04 20:49:18 +0300

    scroll up button

--- composeApp/src/commonMain/kotlin/org/yaabelozerov/investo/ui/main/MainPage.kt
+++ composeApp/src/commonMain/kotlin/org/yaabelozerov/investo/ui/main/MainPage.kt
@@ -1,25 +1,51 @@
 package org.yaabelozerov.investo.ui.main
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.launch
 import org.yaabelozerov.investo.domain.MainViewModel
 
 @Composable
@@ -31,34 +57,57 @@ fun MainPage(
 ) {
     var sharesQuery by remember { mutableStateOf("") }
     val state by mvm.state.collectAsStateWithLifecycle()
-    LazyColumn(
-        state = lazyListState,
-        modifier = modifier.fillMaxSize(),
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        item {
-            AnimatedContent(state.loadingCurrencies) {
-                if (it) {
-                    LinearProgressIndicator(Modifier.fillMaxWidth().padding(horizontal = 8.dp))
-                } else if (state.currencies.isNotEmpty()) {
-                    CurrencyRow(state.currencies)
+    Box(Modifier.fillMaxSize()) {
+        LazyColumn(
+            state = lazyListState,
+            modifier = modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            item {
+                AnimatedContent(state.loadingCurrencies) {
+                    if (it) {
+                        LinearProgressIndicator(Modifier.fillMaxWidth().padding(horizontal = 8.dp))
+                    } else if (state.currencies.isNotEmpty()) {
+                        CurrencyRow(state.currencies)
+                    }
                 }
             }
+            item {
+                ShareSearchBar(
+                    query = sharesQuery,
+                    onUpdateQuery = { sharesQuery = it },
+                    isLoading = state.loadingShares,
+                    onSearch = {
+                        mvm.searchShares(
+                            sharesQuery.trim()
+                        )
+                    },
+                    focusRequester = fr
+                )
+            }
+            items(state.shares, key = { it.figi }) { share -> ShareCard(share) }
         }
-        item {
-            ShareSearchBar(
-                query = sharesQuery,
-                onUpdateQuery = { sharesQuery = it },
-                isLoading = state.loadingShares,
-                onSearch = {
-                    mvm.searchShares(
-                        sharesQuery.trim()
-                    )
-                },
-                focusRequester = fr
-            )
+        val scope = rememberCoroutineScope()
+        val showButton by derivedStateOf { lazyListState.firstVisibleItemIndex > 3 }
+        AnimatedVisibility(
+            showButton,
+            modifier = Modifier.align(Alignment.BottomEnd).offset((-16).dp, (-16).dp),
+            enter = slideInVertically(initialOffsetY = { it }) + fadeIn(),
+            exit = slideOutVertically(targetOffsetY = { it }) + fadeOut()
+        ) {
+            Card( onClick = {
+                scope.launch {
+                    lazyListState.animateScrollToItem(0)
+                }
+            }) {
+                Icon(
+                    Icons.Default.KeyboardArrowUp,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(8.dp).size(32.dp)
+                )
+            }
         }
-        items(state.shares, key = { it.figi }) { share -> ShareCard(share) }
     }
 }
